2eb111ebeb865df60acbfbfc99c98aaa0b9a0d64
Iaphetes <iaphetes@mailfence.com>
2024-12-22 14:19:21 +0100

    started work on list styling

--- src/main.js
+++ src/main.js
@@ -53,3 +53,18 @@ function handleShortcut(event) {
     }
 }
 document.addEventListener("keydown", handleShortcut);
+ // Random tree
+// const N = 300;
+// const gData = {
+//   nodes: [...Array(N).keys()].map(i => ({ id: i })),
+//   links: [...Array(N).keys()]
+//     .filter(id => id)
+//     .map(id => ({
+//       source: id,
+//       target: Math.round(Math.random() * (id-1))
+//     }))
+// };
+// const Graph = new ForceGraph(
+//   (document.getElementById('graph'))
+//     .linkDirectionalParticles(2)
+//     .graphData(gData);
